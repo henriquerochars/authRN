6fe66dc9db7efd9f9318b30ac52d80f590357fc0
Henrique Rocha Serrano <henriquerochaserranoh@gmail.com>
2021-04-03 20:47:44 -0300

    add singOut flow

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, {useContext} from 'react';
+
+import AuthContext from './../contexts/auth';
 
 import AuthRotes from './../routes/auth.routes';
-// import AppRotes from './../routes/app.routes';
+import AppRoutes from './../routes/app.routes';
 
 const Routes: React.FC = () => {
-  return <AuthRotes />;
+  const {signed} = useContext(AuthContext);
+
+  return signed ? <AppRoutes /> : <AuthRotes />;
 };
 
 export default Routes;
--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,4 +1,4 @@
-interface Response {
+export interface Response {
   token: string;
   user: {
     name: string;
